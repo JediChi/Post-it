0ac7ffebd2e809a8478553e1099348a95b44e1b0
Chinyere Akanno <adanmachinyere98@gmail.com>
2023-03-07 14:33:34 +0100

    Created user schema

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,17 @@
+import Joi from "joi";
+
+export const CreateUserSchema = Joi.object({
+  name: Joi.string().min(3).max(20).lowercase().trim().required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .trim()
+    .lowercase()
+    .required(),
+  password: Joi.string()
+    .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+    .min(7)
+    .max(250)
+    .lowercase()
+    .trim()
+    .required(),
+});
