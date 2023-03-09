225217f4cf1528af4161f4c6fdd746118c87b798
Chinyere Akanno <adanmachinyere98@gmail.com>
2023-03-09 17:48:22 +0100

    Added get-postIt service and controllers

--- src/controllers/postit.controller.ts
+++ src/controllers/postit.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import mongoose, { Types } from "mongoose";
 import postitService from "../services/postit.service";
 
 class PostController {
@@ -14,6 +15,29 @@ class PostController {
       success: true,
       message: "Post created successfully",
       data: newPost,
+    });
+  }
+
+  async getOne(req: Request, res: Response) {
+    const _id  = new mongoose.Types.ObjectId(req.params.id)
+    const author = new mongoose.Types.ObjectId(req.user._id)
+    const post = await postitService.getPostById(_id, author);
+
+    return res.status(200).send({
+      success: true,
+      message: "Post fetched successfully",
+      data: post,
+    });
+  }
+
+  async getAllPosts(req: Request, res: Response) {
+    const author = new mongoose.Types.ObjectId(req.user._id);
+    const posts = await postitService.getAllPosts(author);
+
+    return res.status(200).send({
+      success: true,
+      message: "Posts fetched successfully",
+      data: posts,
     });
   }
 }
--- src/services/postit.service.ts
+++ src/services/postit.service.ts
@@ -1,14 +1,25 @@
+import { Types } from "mongoose";
 import { ICreatePost } from "../interfaces/postit.interface";
 import Post from "../models/postit.model";
 
 class PostService {
-    async createPost(data: ICreatePost) {
-        const post = new Post(data);
+  async createPost(data: ICreatePost) {
+    const post = new Post(data);
 
-        await post.save();
+    await post.save();
 
-        return post;
-    }
+    return post;
+  }
+
+  async getPostById(_id: Types.ObjectId, _author: Types.ObjectId) {
+    const post = await Post.findOne({ _id, _author, isDeleted: false });
+    return post;
+  }
+
+  async getAllPosts(_author: Types.ObjectId) {
+    const posts = await Post.find({ isDeleted: false });
+    return posts;
+  }
 }
 
 export default new PostService();
